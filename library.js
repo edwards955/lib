c22b0343cf729239ff81e01b8681b3516221120b
edwards955 <getignorantfriday@gmail.com>
2023-11-27 12:40:44 -0500

    Build functions to display updated library

--- library.js
+++ library.js
@@ -18,8 +18,39 @@ function addBookToLibrary(title, author, pages, read) {
 
 // Display the library
 
-const library = document.querySelector('.library');
+const libraryBody = document.querySelector('.library-body');
 
 function displayLibrary() {
+  const oldLibrary = document.querySelector('.library-body');
+  const newLibrary = document.createElement('tbody');
+  newLibrary.classList.toggle('library-body');
   
+  myLibrary.forEach(book => {
+    // Create row for book info
+    let row = document.createElement('tr');
+    
+    // Create cells for book info
+    let title = document.createElement('td');
+    title.textContent = book.title;
+    let author = document.createElement('td');
+    author.textContent = book.author;
+
+    let pages = document.createElement('td');
+    pages.textContent = book.pages;
+
+    let read = document.createElement('td');
+    read.textContent = book.read;
+
+
+    // Attach cells to the row
+    row.appendChild(title);
+    row.appendChild(author);
+    row.appendChild(pages);
+    row.appendChild(read);
+
+    // Attach row to the table body
+    newLibrary.appendChild(row);
+  })
+
+  oldLibrary.parentNode.replaceChild(newLibrary, oldLibrary);
 }
